fac5c76eb7bc1d288a32fc33628d2d9055ede89b
SushipingMcMeow <69446836+SushipingMcMeow@users.noreply.github.com>
2021-09-30 09:53:35 +1000

    isolate heart beat function
    Give an r peak, it'll select all ecg data points between the 2 surrounding p peaks.

--- ECG_for_biometricsv2.py
+++ ECG_for_biometricsv2.py
@@ -30,40 +30,61 @@ def shrinkTo(data,amount):
     print("new data length:", len(data))
     return data
 #testing method used to select two different heart beats in a sequence
-def selectHeartBeats(data,rpeaks,ppeaks, tpeaks):
-    RPeakA = random.randint(1, len(rpeaks)-1)
+def selectRpeakRange(data,rpeaks,ppeaks, tpeaks):
+    RPeakA = random.randint(1, len(ppeaks)-1)
     RPeakB = -1
     while(RPeakB == -1):
         newRPeak = random.randint(1,len(rpeaks)-1)
         if (newRPeak != RPeakA):
             RPeakB = newRPeak
+#gets data between both P peaks surounding an R peak
+def selectHeartBeat(data, rpeak, ppeaks):
+    ppeakInQuestion = len(ppeaks) - 1
+    ppeaka = -1
+    while(ppeaka == -1):
+        if (ppeaks[ppeakInQuestion] < rpeak):
+            ppeaka = ppeakInQuestion
+        else:
+            ppeakInQuestion -= 1
+    ppeakb = -1
+    ppeakInQuestion = 0
+    while(ppeakb == -1):
+        if (ppeaks[ppeakInQuestion] > rpeak):
+            ppeakb = ppeakInQuestion
+            print("ppeak in question",ppeaks[ppeakInQuestion])
+        else:
+            ppeakInQuestion += 1
+    data = data[ppeaks[ppeaka]:-(len(data)-ppeaks[ppeakb])]
+    print("selected heart beat length: ",len(data))
+    return data
     
-    
-sampling_freq=500
+
+sampling_freq=1000
 ecg_signal=np.loadtxt(".\CYBHi\data\long-term\\20120430-PES-A0-35.txt")
 ecg_signal = shrinkTo(ecg_signal,10000)
 ecg_signal_cleared = ecg_clean(ecg_signal, sampling_freq)
-ecg_signal = ecg_signal_cleared
+#ecg_signal = ecg_signal_cleared
 #ecg_signal=np.loadtxt("20120106-AA-A0-35_small.txt")
 
 ecg_signal_short=ecg_signal[:100]
 timestr = time.strftime("%Y%m%d-%H%M%S")
-#plt.plot(ecg_signal[:200])
-#plt.plot(ecg_signal_short)
-#plt.savefig(timestr+'.jpg',format='jpg', dpi=300)
 
 _, rpeaks = nk.ecg_peaks(ecg_signal, sampling_rate=sampling_freq)
 signals, waves_peak = nk.ecg_delineate(ecg_signal, rpeaks, sampling_rate=sampling_freq)
-plt(ecg_signal_cleared)
-plt(ecg_signal)
-
 signals, info = nk.ecg_process(ecg_signal, sampling_rate=sampling_freq)
+rndhb = selectHeartBeat(ecg_signal_cleared, rpeaks['ECG_R_Peaks'][3],waves_peak['ECG_P_Peaks'])
 print("rpeak type is",rpeaks)
 print(type(signals))
 print(len(signals))
 print(signals)
 print(ecg_signal)
 print(len(ecg_signal))
-plot = nk.events_plot(rpeaks['ECG_R_Peaks'], ecg_signal_cleared)
+print(waves_peak)
+print("selected heart beat:", rndhb)
+plotb = plt.plot(rndhb)
+plot = nk.events_plot([waves_peak['ECG_T_Peaks'],
+                       waves_peak['ECG_P_Peaks'],
+                       waves_peak['ECG_Q_Peaks'],
+                       waves_peak['ECG_S_Peaks']], ecg_signal_cleared)
 
 _, rpeaks = nk.ecg_peaks(ecg_signal, sampling_rate=sampling_freq)
