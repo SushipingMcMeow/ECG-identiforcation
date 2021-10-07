e7b2ebc98c74a38fb8e509f8cd7bce9d5502e0e8
SushipingMcMeow <69446836+SushipingMcMeow@users.noreply.github.com>
2021-10-07 20:49:39 +1100

    Add files via upload

--- ECG_for_biometricsv2.py
+++ ECG_for_biometricsv2.py
@@ -29,14 +29,7 @@ def shrinkTo(data,amount):
     data = np.delete(data,cutoffIndex)
     print("new data length:", len(data))
     return data
-#testing method used to select two different heart beats in a sequence
-def selectRpeakRange(data,rpeaks,ppeaks, tpeaks):
-    RPeakA = random.randint(1, len(ppeaks)-1)
-    RPeakB = -1
-    while(RPeakB == -1):
-        newRPeak = random.randint(1,len(rpeaks)-1)
-        if (newRPeak != RPeakA):
-            RPeakB = newRPeak
+
 #gets data between both P peaks surounding an R peak
 def selectHeartBeat(data, rpeak, ppeaks):
     ppeakInQuestion = len(ppeaks) - 1
@@ -57,13 +50,16 @@ def selectHeartBeat(data, rpeak, ppeaks):
     data = data[ppeaks[ppeaka]:-(len(data)-ppeaks[ppeakb])]
     print("selected heart beat length: ",len(data))
     return data
-    
+def rpeaktorpeak( data,rpeaks, rpeak):
+    data = data[rpeaks[rpeak]:rpeaks[rpeak+1]]
+    return data
 
 sampling_freq=1000
-ecg_signal=np.loadtxt(".\CYBHi\data\long-term\\20120430-PES-A0-35.txt")
+ecg_signal=np.loadtxt(".\CYBHi\data\long-term\\20120111-AG-A0-35.txt")
 ecg_signal = shrinkTo(ecg_signal,10000)
 ecg_signal_cleared = ecg_clean(ecg_signal, sampling_freq)
-#ecg_signal = ecg_signal_cleared
+#ecg_signal = ecg_clean(ecg_signal, sampling_freq)
+ecg_signal = ecg_signal_cleared
 #ecg_signal=np.loadtxt("20120106-AA-A0-35_small.txt")
 
 ecg_signal_short=ecg_signal[:100]
@@ -72,7 +68,17 @@ timestr = time.strftime("%Y%m%d-%H%M%S")
 _, rpeaks = nk.ecg_peaks(ecg_signal, sampling_rate=sampling_freq)
 signals, waves_peak = nk.ecg_delineate(ecg_signal, rpeaks, sampling_rate=sampling_freq)
 signals, info = nk.ecg_process(ecg_signal, sampling_rate=sampling_freq)
-rndhb = selectHeartBeat(ecg_signal_cleared, rpeaks['ECG_R_Peaks'][3],waves_peak['ECG_P_Peaks'])
+#rndhb = selectHeartBeat(ecg_signal_cleared, rpeaks['ECG_R_Peaks'][3],waves_peak['ECG_P_Peaks'])
+#tmphb = selectHeartBeat(ecg_signal_cleared, rpeaks['ECG_R_Peaks'][5],waves_peak['ECG_P_Peaks'])
+rndhb = rpeaktorpeak(ecg_signal_cleared, rpeaks['ECG_R_Peaks'],1)
+tmphb = rpeaktorpeak(ecg_signal_cleared, rpeaks['ECG_R_Peaks'],len(rpeaks['ECG_R_Peaks'])-2)
+alignment = dtw(rndhb, tmphb,keep_internals=True)
+alignment.plot(type="threeway")
+dtw(rndhb, tmphb, keep_internals=True, 
+    step_pattern=rabinerJuangStepPattern(6, "c"))\
+    .plot(type="twoway",offset=-2)
+print(rabinerJuangStepPattern(6,"c"))
+rabinerJuangStepPattern(6,"c").plot()
 print("rpeak type is",rpeaks)
 print(type(signals))
 print(len(signals))
